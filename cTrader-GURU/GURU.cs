ab07d79dc3909199d1469645a4ba160774da5c4b
Leonardo <leonardo.ciaccio@gmail.com>
2018-07-17 20:25:53 +0200

    Starter

---
+++ cTrader-GURU/GURU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cTrader_GURU
+{
+
+    public class GURU
+    {
+
+        public string ciao()
+        {
+
+            return "Hello World !";
+
+        }
+
+    }
+
+    public static class GURUs
+    {
+
+        public static string ciao()
+        {
+
+            return "Hello Worlds !";
+
+        }
+
+    }
+
+}
